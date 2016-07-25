45d6c9f068c06f5b69c255f66bd71a804415fe15
Jack Copsey <jack.copsey@yahoo.com>
2016-07-25 09:56:05 +0100

    Add initial CLI implementation

---
+++ cli/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+#include "../interface/console.hpp"
+
+void print_output(const maf::Console & console) {
+   for (auto & styled_string: console.output()) {
+      switch(styled_string.style) {
+         case maf::Styled_string::Style::game_title:
+         case maf::Styled_string::Style::help_title:
+            // ignore titles
+            break;
+         case maf::Styled_string::Style::command:
+            std::cout << '\'' << styled_string.string << '\'';
+            break;
+         default:
+            std::cout << styled_string.string;
+            break;
+      }
+   }
+
+   std::cout << "\n\n";
+}
+
+void print_error_message(const maf::Console & console) {
+   for (auto & styled_string: console.error_message()) {
+      switch(styled_string.style) {
+         case maf::Styled_string::Style::game_title:
+         case maf::Styled_string::Style::help_title:
+            // ignore titles
+            break;
+         case maf::Styled_string::Style::command:
+            std::cerr << '\'' << styled_string.string << '\'';
+            break;
+         default:
+            std::cerr << styled_string.string;
+            break;
+      }
+   }
+
+   std::cerr << "\n\n";
+}
+
+int main() {
+   maf::Console console{};
+   print_output(console);
+
+   for (bool quit = false; !quit; ) {
+      std::cout << ">> ";
+
+      std::string input;
+      std::getline(std::cin, input);
+
+      std::cout << '\n';
+
+      if (input == "quit") {
+         quit = true;
+      } else if (console.input(input)) {
+         print_output(console);
+      } else {
+         print_error_message(console);
+      }
+   }
+}
