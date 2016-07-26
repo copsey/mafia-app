937812200f8f5d523e214c5436caa304976b6a10
Jack Copsey <jack.copsey@yahoo.com>
2016-07-26 10:17:18 +0100

    Refactor rulebook.hpp
    This includes rewriting the documentation.

--- logic/rulebook.hpp
+++ logic/rulebook.hpp
@@ -1,132 +1,183 @@
 #ifndef MAFIA_LOGIC_RULEBOOK
 #define MAFIA_LOGIC_RULEBOOK
 
+#include <map>
+#include <string>
+#include <vector>
+
 #include "../riketi/ref.hpp"
 
+#include "role.hpp"
 #include "wildcard.hpp"
 
 namespace maf {
    struct Rulebook {
-      // An edition of the rules.
+      /// Edition of the rules.
+      ///
+      /// Although not used at the moment, this primarily exists for supporting
+      /// saved games which may have been created with a rulebook incompatible
+      /// with the most recent rulebook.
       using Edition = int;
-      // The most recent edition of the rules.
+
+      /// The most recent edition of the rules.
       static constexpr Edition latest_edition{1};
 
-      // An exception signifying that no rulebook with the given edition exists.
+      /// Exception signifying that no rulebook with the given edition exists.
       struct Bad_edition {
          Edition edition;
       };
 
-      // Signifies that the rulebook doesn't contain a role with the given ID.
+      /// Exception signifying that no role could be found with the given ID.
       struct Missing_role_ID {
          Role::ID id;
       };
 
-      // Signifies that the rulebook doesn't contain a role with the given
-      // alias.
-      // (this could be because the alias doesn't correspond to a role ID.)
+      /// Exception signifying that no role could be found with the given alias.
+      ///
+      /// This could be because the alias doesn't correspond to a role ID.
       struct Missing_role_alias {
          std::string alias;
       };
 
-      // Signifies that the rulebook doesn't contain a wildcard with the given
-      // ID.
+      /// Exception signifying that no wildcard could be found with
+      /// the given ID.
       struct Missing_wildcard_ID {
          Wildcard::ID id;
       };
 
-      // Signifies that the rulebook doesn't contain a wildcard with the given
-      // alias.
-      // (this could be because the alias doesn't correspond to a wildcard ID.)
+      /// Exception signifying that no wildcard could be found with the
+      /// given alias.
+      ///
+      /// This could be because the alias doesn't correspond to a wildcard ID.
       struct Missing_wildcard_alias {
          std::string alias;
       };
 
-      // Creates a rulebook of the latest edition.
+      /// Make a rulebook with the latest edition.
       Rulebook()
        : Rulebook{latest_edition} { }
 
-      // Creates a rulebook of the specified edition.
+      /// Make a rulebook with the specified edition.
       Rulebook(Edition edition);
 
-      // Gets the edition of the rules being used.
+      /// The edition of the rules being used.
       Edition edition() const {
          return _edition;
       }
 
-      // A vector containing every role defined in the rulebook.
+      /// A vector containing every role defined in the rulebook.
       const std::vector<Role> & roles() const {
          return _roles;
       }
 
-      // A vector containing every village role defined in the rulebook.
+      /// A vector containing every village role defined in the rulebook.
       std::vector<rkt::ref<const Role>> village_roles() const;
-      // A vector containing every mafia role defined in the rulebook.
+
+      /// A vector containing every mafia role defined in the rulebook.
       std::vector<rkt::ref<const Role>> mafia_roles() const;
-      // A vector containing every freelance role defined in the rulebook.
+
+      /// A vector containing every freelance role defined in the rulebook.
       std::vector<rkt::ref<const Role>> freelance_roles() const;
 
-      // A vector containing every wildcard defined in the rulebook.
+      /// A vector containing every wildcard defined in the rulebook.
       const std::vector<Wildcard> & wildcards() const {
          return _wildcards;
       }
 
-      // A vector containing every village-only wildcard defined in the
-      // rulebook.
+      /// A vector containing every village-only wildcard defined in the
+      /// rulebook.
       std::vector<rkt::ref<const Wildcard>> village_wildcards() const;
-      // A vector containing every mafia-only wildcard defined in the rulebook.
+
+      /// A vector containing every mafia-only wildcard defined in the rulebook.
       std::vector<rkt::ref<const Wildcard>> mafia_wildcards() const;
-      // A vector containing every freelance-only wildcard defined in the
-      // rulebook.
+
+      /// A vector containing every freelance-only wildcard defined in the
+      /// rulebook.
       std::vector<rkt::ref<const Wildcard>> freelance_wildcards() const;
 
-      // Whether the rulebook contains a role with the given ID.
+      /// Whether the rulebook contains a role with the given ID.
       bool contains_role(Role::ID id) const;
-      // Whether the rulebook contains a role with the given alias.
-      bool contains_role(const std::string &alias) const;
-      // Whether the rulebook contains a wildcard with the given ID.
+
+      /// Whether the rulebook contains a role with the given alias.
+      bool contains_role(const std::string & alias) const;
+
+      /// Whether the rulebook contains a wildcard with the given ID.
       bool contains_wildcard(Wildcard::ID id) const;
-      // Whether the rulebook contains a wildcard with the given alias.
-      bool contains_wildcard(const std::string &alias) const;
 
-      // Gets the role with the given ID.
-      // Throws an exception if none could be found.
+      /// Whether the rulebook contains a wildcard with the given alias.
+      bool contains_wildcard(const std::string & alias) const;
+
+      /// Get the role with the given ID.
+      ///
+      /// @throws `Missing_role_ID` if none could be found.
       Role & get_role(Role::ID id);
-      const Role & get_role(Role::ID id) const;
-      // Gets the role with the given alias.
-      // Throws an exception if none could be found.
-      Role & get_role(const std::string &alias);
-      const Role & get_role(const std::string &alias) const;
-      // Gets the wildcard with the given ID.
-      // Throws an exception if none could be found.
-      Wildcard & get_wildcard(Wildcard::ID id);
-      const Wildcard & get_wildcard(Wildcard::ID id) const;
-      // Gets the wildcard with the given alias.
-      // Throws an exception if none could be found.
-      Wildcard & get_wildcard(const std::string &alias);
-      const Wildcard & get_wildcard(const std::string &alias) const;
 
-      // Creates and stores a blank role with the given ID.
-      // A reference to the role is returned.
+      /// Get the role with the given ID.
+      ///
+      /// @throws `Missing_role_ID` if none could be found.
+      const Role & get_role(Role::ID id) const;
+
+      /// Get the role with the given alias.
+      ///
+      // @throws `Missing_role_alias` if none could be found.
+      Role & get_role(const std::string & alias);
+
+      /// Get the role with the given alias.
+      ///
+      // @throws `Missing_role_alias` if none could be found.
+      const Role & get_role(const std::string & alias) const;
+
+      /// Get the wildcard with the given ID.
+      ///
+      /// @throws `Missing_wildcard_ID` if none could be found.
+      Wildcard & get_wildcard(Wildcard::ID id);
+
+      /// Get the wildcard with the given ID.
+      ///
+      /// @throws `Missing_wildcard_ID` if none could be found.
+      const Wildcard & get_wildcard(Wildcard::ID id) const;
+
+      /// Get the wildcard with the given alias.
+      ///
+      /// @throws `Missing_wildcard_alias` if none could be found.
+      Wildcard & get_wildcard(const std::string & alias);
+
+      /// Get the wildcard with the given alias.
+      ///
+      /// @throws `Missing_wildcard_alias` if none could be found.
+      const Wildcard & get_wildcard(const std::string & alias) const;
+
+      /// Create and store a new role with the given ID.
+      ///
+      /// The traits of the role are set to their default values.
+      ///
+      /// @returns a reference to the new role.
       Role & new_role(Role::ID id);
-      // Creates and stores a role with the given ID and default village traits.
-      // A reference to the role is returned.
+
+      /// Create and store a new role with the given ID, and default village traits.
+      ///
+      /// @returns a reference to the new role.
       Role & new_village_role(Role::ID id);
-      // Creates and stores a role with the given ID and default Mafia traits.
-      // A reference to the role is returned.
+
+      /// Create and store a new role with the given ID, and default mafia traits.
+      ///
+      /// @returns a reference to the new role.
       Role & new_mafia_role(Role::ID id);
-      // Creates and stores a role with the given ID and default freelance
-      // traits.
-      // A reference to the role is returned.
+
+      /// Create and store a new role with the given ID, and default freelance traits.
+      ///
+      /// @returns a reference to the new role.
       Role & new_freelance_role(Role::ID id);
-      // Creates and stores a wildcard with the given ID and role evaluator.
-      // A reference to the wildcard is returned.
-      Wildcard & new_wildcard(Wildcard::ID id,
-                              Wildcard::Role_evaluator evaluator);
-      // Creates and stores a wildcard with the given ID and role weights.
-      // A reference to the wildcard is returned.
-      Wildcard & new_wildcard(Wildcard::ID id,
-                              const std::map<Role::ID, double> &weights);
+
+      /// Create and store a new wildcard with the given ID and role evaluator.
+      ///
+      /// @returns a reference to the new wildcard.
+      Wildcard & new_wildcard(Wildcard::ID id, Wildcard::Role_evaluator evaluator);
+
+      /// Create and store a new wildcard with the given ID and role weights.
+      ///
+      /// @returns a reference to the new wildcard.
+      Wildcard & new_wildcard(Wildcard::ID id, const std::map<Role::ID, double> & weights);
 
    private:
       Edition _edition;
