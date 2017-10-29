f0f7bd37c422ed5c282ee88b02e242880e8b2b7c
Jack Copsey <jack.copsey@yahoo.com>
2017-10-29 07:34:12 +0000

    Add screen::Peddle_Use

--- interface/game_screens.cpp
+++ interface/game_screens.cpp
@@ -770,6 +770,66 @@ maf::Help_Screen * maf::screen::Investigate_Use::get_help_screen() const {
 
 
 /*
+ * maf::screen::Peddle_Use
+ */
+
+bool maf::screen::Peddle_Use::handle_commands(const std::vector<std::string> & commands) {
+   if (Game_Screen::handle_commands(commands)) return true;
+
+   auto& glog = game_log();
+
+   if (commands_match(commands, {"target", ""})) {
+      if (_page == 0) {
+         const Player & caster = *_caster_ref;
+         const Player & target = glog.find_player(commands[1]);
+
+         glog.cast_peddle(caster.id(), target.id());
+         _page ++;
+      } else {
+         return false;
+      }
+   } else if (commands_match(commands, {"skip"})) {
+      if (_page == 0) {
+         const Player & caster = *_caster_ref;
+
+         glog.skip_peddle(caster.id());
+         _page ++;
+      } else {
+         return false;
+      }
+   } else if (commands_match(commands, {"ok"})) {
+      if (_page == 1) glog.advance();
+      else return false;
+   } else {
+      return false;
+   }
+
+   return true;
+}
+
+void maf::screen::Peddle_Use::write(std::ostream & os) const {
+   auto& glog = game_log();
+
+   const Player & caster = *_caster_ref;
+   auto& caster_name = glog.get_name(caster);
+
+   os << "^TPeddle^/";
+
+   if (_page == 1) {
+      os << caster_name << " should now go back to sleep.^h\n\nWhen you are ready, enter ^cok^/ to continue.";
+   } else {
+      os << caster_name << ", you can choose to peddle drugs to somebody this night.^h\n\nEnter ^ctarget A^/ to peddle drugs to player ^cA^/, or enter ^cskip^/ if you don't wish to peddle drugs to anybody.";
+   }
+}
+
+maf::Help_Screen * maf::screen::Peddle_Use::get_help_screen() const {
+   // FIXME: decide whether or not to include a help screen
+   return nullptr;
+}
+
+
+
+/*
  * maf::screen::Investigation_Result
  */
 
