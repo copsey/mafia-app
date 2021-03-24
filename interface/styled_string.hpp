67409081c306ab5af52673ae348f3591a79c05f7
Jack Copsey <jack.copsey@yahoo.com>
2021-03-24 09:58:52 +0000

    Add support for ranges to "is_param_name"

--- interface/styled_string.hpp
+++ interface/styled_string.hpp
@@ -11,9 +11,42 @@ namespace maf {
 	// '^' replaced with '^^'.
 	std::string escape_tags(std::string_view str);
 	
+	// Check if the range of characters in {i,j} gives an allowed name
+	// for a styled-text parameter.
+	// This is true if it matches the regex /^[a-zA-Z0-9_\.]+$/.
+	//
+	// @pre {i,j} is a valid range.
+	template <typename Iter>
+	bool is_param_name(Iter i, Iter j)
+	{
+		if (i == j) return false;
+		
+		for (; i != j; ++i) {
+			switch (*i) {
+				case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h':
+				case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p':
+				case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x':
+				case 'y': case 'z': case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+				case 'G': case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+				case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V':
+				case 'W': case 'X': case 'Y': case 'Z': case '0': case '1': case '2': case '3':
+				case '4': case '5': case '6': case '7': case '8': case '9': case '_': case '.':
+					break;
+				
+				default:
+					return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	// Check if `param` is an allowed name for a styled-text parameter.
 	// This is true if it matches the regex /^[a-zA-Z0-9_\.]+$/.
-	bool is_param_name(std::string_view param);
+	inline bool is_param_name(std::string_view param)
+	{
+		return is_param_name(param.begin(), param.end());
+	}
 
 	// A string coupled with a suggested style.
 	// A style is intended to entail such formatting properties as typeface,
