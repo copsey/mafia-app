7e3d5c9216273d33be05e3ca640470fcf62264d9
Jack Copsey <jack.copsey@yahoo.com>
2021-04-27 10:55:52 +0100

    util: Fix bug in 'enable_if'

--- util/type_traits.hpp
+++ util/type_traits.hpp
@@ -5,7 +5,7 @@
 
 namespace maf {
 	template <bool B, class T = void>
-	using enable_if = typename std::enable_if_t<B,T>::type;
+	using enable_if = std::enable_if_t<B,T>;
 
 	template <class B, class D>
 	constexpr bool is_base_of = std::is_base_of_v<B,D>;
